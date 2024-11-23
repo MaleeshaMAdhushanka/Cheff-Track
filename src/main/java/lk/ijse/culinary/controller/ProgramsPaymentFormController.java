9491a6e5b3de7dc9888742ef19210cdb4ec5d729
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2024-11-24 01:41:09 +0530

    Creating some fxml files

---
+++ src/main/java/lk/ijse/culinary/controller/ProgramsPaymentFormController.java
@@ -0,0 +1,58 @@
+package lk.ijse.culinary.controller;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ProgramsPaymentFormController {
+
+    @FXML
+    private MFXButton btnRemove;
+
+    @FXML
+    private MFXButton btnRemove1;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colContact;
+
+    @FXML
+    private TableColumn<?, ?> colDob;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableView<?> tblUser;
+
+    @FXML
+    private MFXTextField txtSearch;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRemoveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+}
+
---
+++ src/main/java/lk/ijse/culinary/controller/programsPaymentDataFormController.java
@@ -0,0 +1,40 @@
+package lk.ijse.culinary.controller;
+
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class programsPaymentDataFormController {
+
+    @FXML
+    private MFXButton btnAdd;
+
+    @FXML
+    private MFXButton btnCancel;
+
+    @FXML
+    private MFXTextField contact;
+
+    @FXML
+    private Label lblPayID;
+
+    @FXML
+    private MFXTextField txtEmail;
+
+    @FXML
+    private MFXTextField txtName;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCancelOnAction(ActionEvent event) {
+
+    }
+
+}
