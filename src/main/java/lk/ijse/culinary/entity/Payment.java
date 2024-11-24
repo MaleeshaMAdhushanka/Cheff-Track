b5c721f8e9d1eb3c667bdba2e8706a410a1759f1
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2024-11-24 16:32:07 +0530

    Create Payment adn Student_program  Entity

--- src/main/java/lk/ijse/culinary/entity/Payment.java
+++ src/main/java/lk/ijse/culinary/entity/Payment.java
@@ -1,4 +1,34 @@
 package lk.ijse.culinary.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class Payment {
+
+
+    @Id
+    @Column(name = "pay_id")
+    private String pay_id;
+
+    @Column(name = "pay_date")
+    private String pay_date;
+
+    @Column(name = "pay_amount")
+    private String pay_amount;
+
+    @Column(name = "status")
+    private  String status;
+
+    @Column(name = "upfront_amount")
+    private double upfront_amount;
+
+    @Column(name = "balance_amount")
+    private double balance_amount;
+
+    @ManyToOne
+    @JoinColumn(name = "student_program_id")
+    private Student_Program studentProgram;
+
 }
--- src/main/java/lk/ijse/culinary/entity/Student_Program.java
+++ src/main/java/lk/ijse/culinary/entity/Student_Program.java
@@ -1,4 +1,27 @@
 package lk.ijse.culinary.entity;
 
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "student_program")
 public class Student_Program {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_program_id")
+    private int student_program_id;
+
+    @Column(name = "resgistration_date")
+    private Date registration_date;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "program_id")
+    private Program program;
+
 }
