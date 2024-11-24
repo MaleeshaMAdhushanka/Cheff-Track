6eb4e36b34a33981820aca15f34715cb02090250
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2024-11-24 16:06:11 +0530

    Create Student  Entity

--- src/main/java/lk/ijse/culinary/entity/Student.java
+++ src/main/java/lk/ijse/culinary/entity/Student.java
@@ -1,4 +1,28 @@
 package lk.ijse.culinary.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
 public class Student {
+    @Id
+    @Column(name = "student_id")
+    private String studentId;
+
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+
+    @Column(name = "contact")
+    private String contact;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private  User user;
+
 }
